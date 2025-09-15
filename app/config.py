6ba6d6a6271f77fca5c21ff4d9b1a13ef7188f8f
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-09-16 01:25:50 +0530

    settings file

---
+++ app/config.py
@@ -0,0 +1,36 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    GEMINI_API_KEY: str
+    LANGCHAIN_PROJECT: str
+    OPENAI_API_KEY: str
+    LANGCHAIN_TRACING_V2: bool
+    LANGCHAIN_ENDPOINT: str
+    LANGCHAIN_API_KEY: str
+
+    DATABASE_HOSTNAME: str
+    DATABASE_PORT: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+    DATABASE_USERNAME: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_STARTTLS: bool=True  
+    MAIL_SSL_TLS:bool=False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = False
+
+settings = Settings() #type:ignore
+
