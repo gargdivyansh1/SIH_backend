72ff89739fd164ec7de3141515ad1098d26c0ec6
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-09-16 01:24:43 +0530

    jwt and auth functions

---
+++ app/utils/auth.py
@@ -0,0 +1,62 @@
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.database.database import get_db
+from app.Tables.UserTable import User, UserRole
+from app.utils.jwt import verify_token
+from passlib.context import CryptContext
+import redis
+from typing import Dict
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+redis_client = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload: Dict = verify_token(token)
+        print("Decoded Payload:", payload)  
+
+        user_id = payload.get("user_id")
+        if not user_id:
+            raise credentials_exception
+
+        user = db.query(User).filter(User.id == user_id).first()
+
+        if not user:
+            raise HTTPException(status_code=401, detail="User not found")
+
+        return user
+
+    except HTTPException as e:
+        raise e  
+
+    except Exception as e:
+        print("Unexpected error:", e)  
+        raise credentials_exception
+    
+def admin_required(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = verify_token(token) 
+    
+    user = db.query(User).filter(User.id == payload["user_id"]).first()
+
+    if not user or user.role != UserRole.ADMIN: # type: ignore
+        raise HTTPException(status_code=403, detail="Insufficient permissions")
+    
+    return user  
---
+++ app/utils/jwt.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from fastapi import HTTPException, status
+from typing import Optional
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv('ALGORITHM')
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM) #type:ignore
+
+def verify_token(token: str) -> dict:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) #type:ignore
+        if payload.get("user_id") is None:
+            raise credentials_exception
+        return payload
+    except JWTError:
+        raise credentials_exception
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) # type:ignore
+        return payload
+    except JWTError as e:
+        error_message = str(e)
+        if "Signature has expired" in error_message:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token expired"
+            )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )
