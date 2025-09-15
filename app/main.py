ae3c6afadbbabcd3cd4cb3b9dd4e4d926b1e5d65
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-09-16 01:26:26 +0530

    main file

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.ChatBotRoutes import router as farmer_route  
-from app.routes.YeildPrediction import router as yeidlPrediction
+# from app.routes.YeildPrediction import router as yeidlPrediction
+from app.routes.authRoutes import router as AuthRouter
 from app.database.chatbot_database import init_db
 from app.index.chatbot_index import run_conversation_stream
 from app.routes.CropPrediction import router as CropPrediction
 
-app = FastAPI()
+app = FastAPI(
+    title="KisanMitra",
+    description="Backend for KisanMitra",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,8 +27,9 @@ app.add_middleware(
 init_db()
 
 app.include_router(farmer_route)
-app.include_router(yeidlPrediction)
+# app.include_router(yeidlPrediction)
 app.include_router(CropPrediction)
+app.include_router(AuthRouter)
 
 @app.websocket("/ws/{user_id}/{session_id}")
 async def websocket_endpoint(websocket: WebSocket, user_id: str, session_id: str):
